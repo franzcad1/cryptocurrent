e42d03a1e5ef47fdd676babddc59cc0a4551275f
Franz Cadiente <fcadien1@my.centennialcollege.ca>
2023-09-20 08:13:02 -0400

    created purchased coin component

---
+++ src/components/PurchasedCoin/PurchasedCoin.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+    CoinPortfolioContainer,
+    CoinContainer,
+    PriceAndCoinContainer,
+    IconContainer,
+    CoinName,
+    Label,
+    LabelEditContainer,
+    EditContainer,
+    NumbersContainer,
+    LabelWithNumberContainer,
+    Number,
+    Icon,
+    EditIcon,
+    FullBar,
+    PartialBar,
+  } from "./PurchasedCoin.styles";
+export default function PurchasedCoin() {
+  return (
+    <CoinPortfolioContainer>
+      <CoinContainer>
+        <IconContainer>
+          <Icon src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" />
+        </IconContainer>
+        <CoinName>Bitcoin (BTC)</CoinName>
+      </CoinContainer>
+      <PriceAndCoinContainer>
+        <LabelEditContainer>
+          <Label>Market Price:</Label>
+        </LabelEditContainer>
+        <NumbersContainer>
+          <LabelWithNumberContainer>
+            <Label>Current Price:</Label>
+            <Number>$39,504</Number>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Price Change 24h:</Label>
+            <Number>$39,504</Number>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Market Cap vs Volume:</Label>
+            <Number>44%</Number>
+            <FullBar>
+              <PartialBar width={44} />
+            </FullBar>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Circ Supply vs Max Supply:</Label>
+            <Number>$39,504</Number>
+          </LabelWithNumberContainer>
+        </NumbersContainer>
+        <LabelEditContainer>
+          <Label>Your Coin:</Label>
+          <EditContainer>
+            <EditIcon />
+          </EditContainer>
+        </LabelEditContainer>
+        <NumbersContainer>
+          <LabelWithNumberContainer>
+            <Label>Coin Amount:</Label>
+            <Number>$39,504</Number>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Amount Value:</Label>
+            <Number>$39,504</Number>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Amount price change since purchase:</Label>
+            <Number>10.24%</Number>
+          </LabelWithNumberContainer>
+          <LabelWithNumberContainer>
+            <Label>Purchase Date:</Label>
+            <Number>06.25.23</Number>
+          </LabelWithNumberContainer>
+        </NumbersContainer>
+      </PriceAndCoinContainer>
+    </CoinPortfolioContainer>
+  );
+}
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,24 +1,10 @@
 import React, { Component } from "react";
 import CoinModal from "../../components/CoinModal/CoinModal";
+import PurchasedCoin from "../../components/PurchasedCoin/PurchasedCoin";
 import {
   PortfolioContainer,
   Heading,
   AddButton,
-  CoinPortfolioContainer,
-  CoinContainer,
-  PriceAndCoinContainer,
-  IconContainer,
-  CoinName,
-  Label,
-  LabelEditContainer,
-  EditContainer,
-  NumbersContainer,
-  LabelWithNumberContainer,
-  Number,
-  Icon,
-  EditIcon,
-  FullBar,
-  PartialBar,
 } from "./Portfolio.styles";
 export default class Portfolio extends Component {
   state = {
@@ -65,64 +51,7 @@ export default class Portfolio extends Component {
             Add Asset
           </AddButton>
           <Heading>Your Statistics</Heading>
-          <CoinPortfolioContainer>
-            <CoinContainer>
-              <IconContainer>
-                <Icon src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" />
-              </IconContainer>
-              <CoinName>Bitcoin (BTC)</CoinName>
-            </CoinContainer>
-            <PriceAndCoinContainer>
-              <LabelEditContainer>
-                <Label>Market Price:</Label>
-              </LabelEditContainer>
-              <NumbersContainer>
-                <LabelWithNumberContainer>
-                  <Label>Current Price:</Label>
-                  <Number>$39,504</Number>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Price Change 24h:</Label>
-                  <Number>$39,504</Number>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Market Cap vs Volume:</Label>
-                  <Number>44%</Number>
-                  <FullBar>
-                    <PartialBar width={44} />
-                  </FullBar>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Circ Supply vs Max Supply:</Label>
-                  <Number>$39,504</Number>
-                </LabelWithNumberContainer>
-              </NumbersContainer>
-              <LabelEditContainer>
-                <Label>Your Coin:</Label>
-                <EditContainer>
-                  <EditIcon />
-                </EditContainer>
-              </LabelEditContainer>
-              <NumbersContainer>
-                <LabelWithNumberContainer>
-                  <Label>Coin Amount:</Label>
-                  <Number>$39,504</Number>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Amount Value:</Label>
-                  <Number>$39,504</Number>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Amount price change since purchase:</Label>
-                  <Number>10.24%</Number>
-                </LabelWithNumberContainer>
-                <LabelWithNumberContainer>
-                  <Label>Purchase Date:</Label>
-                  <Number>06.25.23</Number>
-                </LabelWithNumberContainer>
-              </NumbersContainer>
-            </PriceAndCoinContainer>
-          </CoinPortfolioContainer>
+          <PurchasedCoin></PurchasedCoin>
         </PortfolioContainer>
       </>
     );
